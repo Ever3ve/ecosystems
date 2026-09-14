69947cf7b70dc95c64ea15715db123fe3260924a
Ever3ve <eveline.mty@gmail.com>
2026-09-14 21:45:30 +0300

    change menu

--- Program.cs
+++ Program.cs
@@ -71,9 +71,8 @@
 
         private static void ShowMenu()
         {
-            Console.WriteLine("Welcome to the Calculator App!");
+            Console.WriteLine("Welcome to the Calculator App! by the girls xx");
             Console.WriteLine("-------------------------------");
-            
             Console.WriteLine("Please choose an operation:");
             Console.WriteLine("1. Addition");
             Console.WriteLine("2. Subtraction");
